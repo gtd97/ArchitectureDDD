06846b5b06323417109cfcf23d622326eb6f79b9
gtd97 <genistorres97@gmail.com>
2018-07-18 12:07:30 +0200

    update

--- ExamenVueling.Facade.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ExamenVueling.Facade.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ExamenVueling.Facade.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamenVueling.Facade.Api.WebApiApplication" Language="C#" %>
