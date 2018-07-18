04fd27ee727987d31c49dffe7f11b76a442584f3
gtd97 <genistorres97@gmail.com>
2018-07-18 12:05:02 +0200

    update

---
+++ ExamenVueling.Domain.Models/AlumnoEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenVueling.Domain.Models
+{
+    public class AlumnoEntity
+    {
+        public partial class Alumno
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public string Dni { get; set; }
+            public DateTime Fecha { get; set; }
+            public int Edad { get; set; }
+        }
+    }
+}
---
+++ ExamenVueling.Infrastructure.Repository/Repository/AlumnoRepository.cs
@@ -0,0 +1,40 @@
+﻿using ExamenVueling.Domain.Models;
+using ExamenVueling.Infrastructure.Repository.Contracts;
+using ExamenVueling.Infrastructure.Repository.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenVueling.Infrastructure.Repository.Repository
+{
+    public class AlumnoRepository : IRepository<AlumnoEntity>
+    {
+        public AlumnoEntity Add(AlumnoEntity model)
+        {
+            // Aqui se haria el automapper para pasar a objeto 
+            throw new NotImplementedException();
+        }
+
+        public List<AlumnoEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AlumnoEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AlumnoEntity Update(AlumnoEntity model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
